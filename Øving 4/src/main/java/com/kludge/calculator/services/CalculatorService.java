98f98c0f72ac88071739eabf35cec61e3cdb7415
Andreas <andreksv@ntnu.no>
2024-02-14 10:33:41 +0100

    Initial commit for exercise 4

---
+++ Øving 4/src/main/java/com/kludge/calculator/services/CalculatorService.java
@@ -0,0 +1,11 @@
+package main.java.com.kludge.calculator.services;
+
+import main.java.com.kludge.calculator.api.model.Calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public Calculator getCalculator(Integer id) {
+
+    }
+}
