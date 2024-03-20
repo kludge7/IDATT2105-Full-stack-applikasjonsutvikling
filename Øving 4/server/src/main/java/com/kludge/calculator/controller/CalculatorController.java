50dd962d204c27eda252e44b373bf75d690124a7
Andreas <andreksv@ntnu.no>
2024-03-20 12:13:40 +0100

    Removed unnecessary lines

--- Øving 4/server/src/main/java/com/kludge/calculator/controller/CalculatorController.java
+++ Øving 4/server/src/main/java/com/kludge/calculator/controller/CalculatorController.java
@@ -2,7 +2,6 @@ package com.kludge.calculator.controller;
 
 import com.kludge.calculator.model.CalculationResult;
 import com.kludge.calculator.services.CalculatorService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +12,6 @@ import org.slf4j.LoggerFactory;
 public class CalculatorController {
     private final CalculatorService calculatorService;
     Logger logger = LoggerFactory.getLogger(CalculatorController.class);
-    @Autowired
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
